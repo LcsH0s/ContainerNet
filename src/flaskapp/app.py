14a7605078fa653e20b471cbf4ab3985fc74da5f
LcsH0s <lucasdecastro.web@gmail.com>
2022-02-05 17:56:29 +0100

    api cors bypass

--- src/flaskapp/app.py
+++ src/flaskapp/app.py
@@ -6,8 +6,11 @@ from multiprocessing import context
 import os
 from requests import request
 from flask import Flask, jsonify
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/', methods=['GET'])
 
