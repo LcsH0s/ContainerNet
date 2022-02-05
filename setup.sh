2a4c0bf1d0d53c38be137914b1c0cb4ae265a90b
LcsH0s <lucasdecastro.web@gmail.com>
2022-02-05 18:33:58 +0100

    app cleeanup

--- setup.sh
+++ setup.sh
@@ -1,8 +1,3 @@
-#!/bin/bash
-dir="$(pwd)"
-
 cp -r ./src/flaskapp ./docker/api/
-cp -r ./bots ./docker/api
-cp ./src/web/index.js ./docker/web/public_html/assets/js
 
 docker compose build --pull
--- src/flaskapp/app.py
+++ src/flaskapp/app.py
@@ -8,26 +8,14 @@ from requests import request
 from flask import Flask, jsonify
 from flask_cors import CORS
 
-
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/', methods=['GET'])
 
 def def_root():
-    return "main but there is nothing here lol"
+    return "ddx manager api"
 
-@app.route('/api/', methods=['GET'])
-
-def def_api_root():
-    return "api"
-
-
-@app.route('/dashboard/', methods=['GET'])
-
-def def_dashboard_root():
-    index = open('flaskapp/templates/index.html')
-    return index.read()
 
 if __name__ == '__main__':
       app.run(host='0.0.0.0', port=5050, ssl_context=('/flaskapp/certs/cert.pem', '/flaskapp/certs/key.pem'), debug=True)
