c47c4fbde17d1fda7016c9b337848ba2e742e901
LcsH0s <lucasdecastro.web@gmail.com>
2022-02-05 14:50:51 +0100

    index.js src

--- src/web/index.js
+++ src/web/index.js
@@ -1,3 +1,13 @@
+const api = "http://localhost:5050";
+
 window.onload = function () {
   console.log("logged in!");
+
+  fetch(api)
+    .then((data) => {
+      return data.json();
+    })
+    .then((res) => {
+      console.log(res);
+    });
 };
