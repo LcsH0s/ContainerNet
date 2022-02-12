d3909f2fe5347683b5c4ac5066c2bd860eada2b4
LcsH0s <lucasdecastro.web@gmail.com>
2022-02-12 12:37:29 +0100

    botManager lib working & app import

--- docker/api/flaskapp/app.py
+++ docker/api/flaskapp/app.py
@@ -1,31 +1,29 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from crypt import methods
-import json
-from flask import Flask, jsonify
+from flask import Flask
 from flask_cors import CORS
-from botManager import *
-from botManager.botManager import BotManager
+from botManager.botManager import botManager
 
+WORKDIR = ""
 
 app = Flask(__name__)
 CORS(app)
-manager = BotManager()
+manager = botManager()
+
 
 @app.route('/', methods=['GET'])
-
 def def_root():
-    manager.add('test', '/bots/sam_utility/','awdawdawdawdawdawdawdawda')
+    manager.add('test', '/bots/music_test/', 'awdawdawdawdawdawdawdawda')
     manager.start('test')
     return "ddx manager api"
 
-@app.route('/bots/online', methods=['GET'])
 
+@app.route('/bots/online', methods=['GET'])
 def get_online_bots():
     return 'test'
 
 
 if __name__ == '__main__':
-      app.run(host='0.0.0.0', port=5050, ssl_context=('/flaskapp/certs/cert.pem', '/flaskapp/certs/key.pem'), debug=True)
-
+    app.run(host='0.0.0.0', port=5050, ssl_context=(
+        '/flaskapp/certs/cert.pem', '/flaskapp/certs/key.pem'), debug=True)
--- docker/api/flaskapp/botManager/botManager.py
+++ docker/api/flaskapp/botManager/botManager.py
@@ -1,42 +1,94 @@
+import docker
 
-import subprocess
-import sys
+from os import mkdir
+from distutils.dir_util import copy_tree
+
+WORKDIR = '/Users/lucasdecastro/Documents/XPROG/ContainerNet'
 
 
-class Bot():
-    def __init__(self, token:str, name:str, path:str) -> None:
-        self.token = token
+class BotContainer():
+    STATUS_ONLINE = "bot_status_online"
+    STATUS_OFFLINE = "bot_status_offline"
+
+    def __init__(self, client, path: str, name: str, token: str):
+        self.client = client
+        self.container = None
         self.path = path
-        self.name = name
-        self.thread = None
+        self.name = 'ddx-' + name
+        self.docker_context = f'{WORKDIR}/tmp/dockerfiles/{self.name}'
+        self.status = self.STATUS_OFFLINE
+        self.img_name = f'{self.name}_img'
+        self.token = token
+
+    def dockerize(self):
+        try:
+            mkdir(self.docker_context)
+        except FileExistsError:
+            pass
+
+        f = open(f'{self.docker_context}/Dockerfile', 'w')
+        f.write("FROM python:latest\nCOPY ./app /app \nCOPY ./requirements.txt /requirements.txt \nRUN pip install -r /requirements.txt\nCMD python /app/main.py\n")
+        f.close()
+
+        copy_tree(self.path, self.docker_context)
+
+        self.client.images.build(path=self.docker_context, tag=self.img_name)
+
+        try:
+            self.client.containers.get(self.name).remove(force=True)
+        except docker.errors.NotFound:
+            pass
+
+        self.container = self.client.containers.create(
+            image=self.img_name,
+            name=self.name,
+            environment=[f'BOT_TOKEN={self.token}']
+        )
+
+    def start(self):
+        self.container.start()
+
+    def stop(self):
+        self.container.stop()
 
 
 class BotManager():
     def __init__(self) -> None:
-        self.bots = []
-        self.onlineBots = []
-        self.offlineBots = []
-    
-    def add(self, name:str, path:str, token:str) -> None:
-        self.bots.append(Bot(token,name, path))
-    
-    def start(self, name:str) -> None:
-        if (self.getBotbyName(name) != None):
-            bot:Bot = self.getBotbyName(name)
-            bot.thread = subprocess.Popen(['/bin/bash',f'{bot.path}run.sh',f'{bot.path}'])
-        
-    
-    def getNames(self) -> list:
-        print([b.name for b in self.bots])
-        return [b.name for b in self.bots]
-    
-    def getBotbyName(self, name:str) -> Bot:
-        for b in self.bots:
-            if (b.name == name):
-                return b
-        return None
-    
-    def countOnlineBots(self) -> int:
-        return len(self.onlineBots)
-    
-    
+        self.client = docker.from_env()
+        self.bot_containers = {}
+
+    def add(self, context: str, name: str, token: str):
+        if (not self.bot_exists(name)):
+            self.bot_containers[name] = BotContainer(
+                self.client, context, name, token)
+            self.bot_containers[name].dockerize()
+        else:
+            raise(ReferenceError(
+                f"Container with name '{name}' already exists"))
+
+    def start(self, name: str):
+        if (self.bot_exists(name)):
+            self.bot_containers[name].start()
+        else:
+            raise(KeyError(f'No container with name {name}'))
+
+    def stop(self, name: str):
+        if (self.bot_exists(name)):
+            self.bot_containers[name].stop()
+        else:
+            raise(KeyError(f'No container with name {name}'))
+
+    def bot_exists(self, name: str):
+        return True if name in self.bot_containers.keys() else False
+
+
+"""
+    USAGE EXAMPLE:
+        if (__name__ == "__main__"):
+            bm = BotManager()
+            bm.add(context=f'{WORKDIR}/bots/test',
+                name='test',
+                token='OTM5OTc0NzE4MDk2ODE4MTc2.YgAprA.nBwsdDhEmfOoEKHJLGvdmXwsDxg'
+                )
+            bm.start('test')
+"""
